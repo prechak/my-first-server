c2b330a6ed70d8c0ac8cdc7202af005f80d197c5
Prechak <benedict.precha@gmail.com>
2024-06-24 03:57:42 +0700

    complete crud api

--- app.mjs
+++ app.mjs
@@ -6,23 +6,56 @@ const port = 4000;
 
 app.use(express.json());
 
+// app.get("/posts", async (req, res) => {
+//   let result;
+//   try {
+//     result = await connectionPool.query(`select * from posts`);
+//   } catch {
+//     return res.status(500).json({
+//       message: "Server could not read post because database connection",
+//     });
+//   }
+
+//   return res.status(200).json({
+//     data: result.rows,
+//   });
+// });
+
+// query by category and length
+
 app.get("/posts", async (req, res) => {
-  let result;
+  let results;
+  // 1) Access query parameter ที่ client แนบมากับ HTTP Endpoint
+  const category = req.query.category;
+  const length = req.query.length;
+
   try {
-    result = await connectionPool.query(`select * from posts`);
-  } catch {
+    // 2) เขียน query เพื่ออ่านข้อมูลโพสต์ ด้วย connection pool
+    results = await connectionPool.query(
+      `
+      SELECT * FROM posts
+      WHERE
+      (category = $1 OR $1 IS NULL OR $1 = '')
+      AND
+      (length = $2 OR $2 IS NULL OR $2 = '')
+      `,
+      [category, length]
+    );
+  } catch (error) {
+    // Added error parameter to catch block
     return res.status(500).json({
-      message: "Server could not read post because database connection",
+      message: "Server could not read post because of a database issue",
     });
   }
 
+  // 3) Success query
   return res.status(200).json({
-    data: result.rows,
+    data: results.rows,
   });
 });
 
 app.get("/posts/:postId", async (req, res) => {
-  //1) access parameder
+  //1) access parameter
   const postIdFromClient = req.params.postId;
   //2) query
   const result = await connectionPool.query(
@@ -32,7 +65,7 @@ app.get("/posts/:postId", async (req, res) => {
   // ถ้าไม่มีข้อมูล
   if (!result.rows[0]) {
     return res.status(404).json({
-      message: `Server cound not find a requset post (post id: ${postIdFromClient})`,
+      message: `Server couสnd not find a requset post (post id: ${postIdFromClient})`,
     });
   }
 
@@ -72,6 +105,65 @@ app.post("/posts", async (req, res) => {
   return res.status(201).json({
     message: "Created post successfully",
   });
+});
+
+app.put("/posts/:postId", async (req, res) => {
+  try {
+    //1 access endpoint parameter
+    const postIdFromClient = req.params.postId;
+    const updatedPost = { ...req.body, updated_at: new Date() };
+
+    //2 เขียน query
+    await connectionPool.query(
+      `
+      update posts
+      set title = $2,
+          content = $3,
+          category = $4,
+          length = $5,
+          status = $6,
+          updated_at = $7
+      where post_id = $1
+      `,
+      [
+        postIdFromClient,
+        updatedPost.title,
+        updatedPost.content,
+        updatedPost.category,
+        updatedPost.length,
+        updatedPost.status,
+        updatedPost.updated_at,
+      ]
+    );
+
+    return res.status(200).json({
+      message: `Updated post successfully`,
+    });
+  } catch {
+    return res.status(404).json({
+      message: "Server could not find a requested post to update",
+    });
+  }
+});
+
+app.delete("/posts/:postId", async (req, res) => {
+  try {
+    const postIdFromClient = req.params.postId;
+
+    await connectionPool.query(
+      `delete from posts
+      where post_id = $1`,
+      [postIdFromClient]
+    );
+
+    return res.status(200).json({
+      message: "Delete post successfully",
+    });
+  } catch {
+    return res.status(404).json({
+      message: "Server could not find a requested post to delete",
+    });
+  }
 });
 
 app.listen(port, () => {
